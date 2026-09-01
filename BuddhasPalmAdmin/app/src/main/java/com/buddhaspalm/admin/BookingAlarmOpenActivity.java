11b6c1537cd2fa0ddea255212b054bf3054fb721
buddhasmassage-oss <buddhasmassage@gmail.com>
2026-09-01 17:12:53 +0800

    Stop Admin booking alarm when notification is opened

---
+++ BuddhasPalmAdmin/app/src/main/java/com/buddhaspalm/admin/BookingAlarmOpenActivity.java
@@ -0,0 +1,20 @@
+package com.buddhaspalm.admin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class BookingAlarmOpenActivity extends Activity {
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BookingAlarmService.stop(this);
+        int bookingId=getIntent()!=null?getIntent().getIntExtra("booking_id",0):0;
+        Intent open=new Intent(this,MainActivity.class)
+                .setData(Uri.parse("https://metro.buddhaspinas.com/notifications.php"))
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        if(bookingId>0) open.putExtra("booking_id",bookingId);
+        startActivity(open);
+        finish();
+    }
+}
